11577bccf8963e5feb40e16464f669352431e9e0
Jimmy Givans <144649990+Ja-Uyoma@users.noreply.github.com>
2023-11-02 14:26:26 +0300

    Initial commit
    - Added CMakeLists.txt file
    - Added conanfile.py
    - Added LICENSE file
    - Added README.md file
    - Added src/CMakeLists.txt file
    - Added src/main.cpp file

---
+++ conanfile.py
@@ -0,0 +1,13 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+class KiloRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("gtest/1.14.0")
+        self.requires("ms-gsl/4.0.0")
+
+    def layout(self):
+        cmake_layout(self)
