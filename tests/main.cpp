c2fbee70961b9be4c91ae77262de63fc3bdb5aec
Jimmy Givans <jauyoma.dev@gmail.com>
2024-01-22 15:17:35 +0300

    test: add a default test case using Catch2
    - This is used to confirm that our setup is working as intended

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,16 @@
-#include <cstdlib>
+#include <cstdint>
 
-int main()
+#include <catch2/catch_test_macros.hpp>
+
+constexpr std::uint32_t factorial(std::uint32_t number) noexcept
 {
-    return EXIT_SUCCESS;
+    return number <= 1 ? number : factorial(number - 1) * number;
+}
+
+TEST_CASE("Factorials are computed", "[factorial]")
+{
+    REQUIRE(factorial(1) == 1);
+    REQUIRE(factorial(2) == 2);
+    REQUIRE(factorial(3) == 6);
+    REQUIRE(factorial(10) == 3'628'800);
 }
