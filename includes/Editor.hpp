53edf77eb5e5d5a0fa6406ba1769f9361811c132
Jimmy Givans <jauyoma.dev@gmail.com>
2024-03-02 12:33:43 +0300

    feat: add function prototypes for functions in Editor namespace

---
+++ includes/Editor.hpp
@@ -0,0 +1,12 @@
+namespace Kilo::Editor
+{
+    /// @brief Read key input from stdin
+    /// @return The character read
+    /// @throws std::system_error if an error occured during read
+    char readKey();
+
+    /// @brief Process the results from readKey
+    /// @details This function is responsible for mapping keypresses to editor operations
+    /// @throws std::system_error if an error occured during read
+    void processKeypress() noexcept(noexcept(readKey()));
+}
