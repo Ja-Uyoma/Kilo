206dbe5c853fb21b4432f188491fe38fb01e9400
Jimmy Givans <jauyoma.dev@gmail.com>
2024-07-01 17:34:05 +0300

    style: format documents using clang-format

--- tests/Terminal.test.cpp
+++ tests/Terminal.test.cpp
@@ -1,17 +1,17 @@
-/** 
+/**
  * MIT License
  * Copyright (c) 2023 Jimmy Givans
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,29 +26,28 @@
 #include <gtest/gtest.h>
 #include <system_error>
 
-namespace Kilo::Terminal
+namespace Kilo::Terminal {
+class TerminalMode : public ::testing::Test
 {
-    class TerminalMode : public ::testing::Test
-    {
-    public:
-        ::termios term;
+public:
+  ::termios term;
 
-        void TearDown() override
-        {
-            disableRawMode(term);
-        }
-    };
+  void TearDown() override
+  {
+    disableRawMode(term);
+  }
+};
 
-    TEST_F(TerminalMode, enableRawModeSetsTheTerminalDriverToNonCanonicalMode)
-    {
-        try {
-            enableRawMode(term);
-        }
-        catch (std::system_error const& err) {
-            std::cerr << err.what() << '\n';
-            std::exit(EXIT_FAILURE);
-        }
+TEST_F(TerminalMode, enableRawModeSetsTheTerminalDriverToNonCanonicalMode)
+{
+  try {
+    enableRawMode(term);
+  }
+  catch (std::system_error const& err) {
+    std::cerr << err.what() << '\n';
+    std::exit(EXIT_FAILURE);
+  }
 
-        ASSERT_TRUE(ascertainNonCanonicalMode(term));
-    }
+  ASSERT_TRUE(ascertainNonCanonicalMode(term));
 }
+}   // namespace Kilo::Terminal
--- tests/WriteBuffer.test.cpp
+++ tests/WriteBuffer.test.cpp
@@ -1,17 +1,17 @@
-/** 
+/**
  * MIT License
  * Copyright (c) 2023 Jimmy Givans
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,32 +26,31 @@
 #include <cstring>
 #include <gtest/gtest.h>
 
-namespace Kilo
+namespace Kilo {
+class WriteBufferTest : public ::testing::Test
 {
-    class WriteBufferTest : public ::testing::Test
-    {
-    public:
-        WriteBuffer buf;
-    };
+public:
+  WriteBuffer buf;
+};
 
-    TEST_F(WriteBufferTest, IsEmptyWhenCreated)
-    {
-        ASSERT_EQ(buf.size(), 0);
-    }
-
-    TEST_F(WriteBufferTest, ItsSizeIncreasesByTheLengthOfTheAppendedString)
-    {
-        char const* str = "Hello, World!";
-        buf.write(str, std::strlen(str));
-
-        ASSERT_EQ(buf.size(), 13);
-    }
-
-    TEST_F(WriteBufferTest, c_strReturnsACStringRepresentationOfTheContentsOfTheBuffer)
-    {
-        char const* str = "The quick brown fox jumped over the lazy dog";
-        buf.write(str, std::strlen(str));
-        
-        ASSERT_STREQ(buf.c_str(), str);
-    }
+TEST_F(WriteBufferTest, IsEmptyWhenCreated)
+{
+  ASSERT_EQ(buf.size(), 0);
 }
+
+TEST_F(WriteBufferTest, ItsSizeIncreasesByTheLengthOfTheAppendedString)
+{
+  char const* str = "Hello, World!";
+  buf.write(str, std::strlen(str));
+
+  ASSERT_EQ(buf.size(), 13);
+}
+
+TEST_F(WriteBufferTest, c_strReturnsACStringRepresentationOfTheContentsOfTheBuffer)
+{
+  char const* str = "The quick brown fox jumped over the lazy dog";
+  buf.write(str, std::strlen(str));
+
+  ASSERT_STREQ(buf.c_str(), str);
+}
+}   // namespace Kilo
