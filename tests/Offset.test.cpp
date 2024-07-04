212694ac8417209741716aefe9855dda9f07aa49
Jimmy Givans <jauyoma.dev@gmail.com>
2024-07-04 11:19:44 +0300

    test: add test case for Offset struct

---
+++ tests/Offset.test.cpp
@@ -0,0 +1,15 @@
+#include "Offset.hpp"
+
+#include <gtest/gtest.h>
+
+namespace Kilo {
+
+TEST(Offset, MembersAreInitializedToZeroByDefault)
+{
+  Offset off;
+
+  ASSERT_EQ(off.row, 0);
+  ASSERT_EQ(off.col, 0);
+}
+
+}   // namespace Kilo
