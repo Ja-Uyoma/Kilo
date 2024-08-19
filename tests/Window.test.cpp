5bb8b5d9ec0a36e920e1f505e1a9e6d1fd1eee71
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-19 17:34:00 +0300

    test: add tests for Window class accessors

--- tests/Window.test.cpp
+++ tests/Window.test.cpp
@@ -1,5 +1,6 @@
 #include "Window/Window.hpp"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 namespace Kilo::window {
@@ -10,6 +11,20 @@ TEST(Window, MembersAreInitializedToZeroByDefault)
 
   ASSERT_EQ(window.m_rows, 0);
   ASSERT_EQ(window.m_cols, 0);
+}
+
+TEST(Window, RowsReturnsTheNumberOfRows)
+{
+  Window w;
+
+  ASSERT_THAT(w.rows(), ::testing::Eq(0));
+}
+
+TEST(Window, ColsReturnsTheNumberOfColumns)
+{
+  Window w;
+
+  ASSERT_THAT(w.cols(), ::testing::Eq(0));
 }
 
 }   // namespace Kilo::window
