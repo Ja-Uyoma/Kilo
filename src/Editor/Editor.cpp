27c7c5a7a8b7148650f474a0e8c4b3947be7c5f8
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-21 17:05:32 +0300

    refactor: swap numrows == 0 with row.empty()

--- src/Editor/Editor.cpp
+++ src/Editor/Editor.cpp
@@ -145,8 +145,8 @@ void drawRows(ScreenBuffer& buffer) noexcept
   using namespace std::string_literals;
 
   for (int y = 0; y < editorConfig.window.rows(); y++) {
-      if (editorConfig.numrows == 0 && y == editorConfig.window.rows() / 3) {
     if (int filerow = y + editorConfig.off.row; std::cmp_greater_equal(filerow, editorConfig.row.size())) {
+      if (editorConfig.row.empty() && y == editorConfig.window.rows() / 3) {
         detail::printWelcomeMessage(editorConfig.window.cols(), buffer);
       }
       else {
