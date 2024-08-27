7c8bc9b73dfd5802866a4d07a60d3cb2768f4923
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-27 15:46:52 +0300

    refactor: add a return type to the interface of read

--- src/Terminal/System.hpp
+++ src/Terminal/System.hpp
@@ -36,8 +36,9 @@ public:
    * @param fd The file being read from
    * @param buffer The buffer being read to
    * @param nbytes The number of bytes to read
+   * @returns The number of bytes read
    */
-  virtual void read(int fd, void* buffer, std::size_t nbytes) = 0;
+  virtual std::size_t read(int fd, void* buffer, std::size_t nbytes) = 0;
 
   /**
    * @brief Write n bytes of buffer to fd
