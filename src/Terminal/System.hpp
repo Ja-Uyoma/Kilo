a5bc6225e0fa2b9c50713ced96850ec864bdba5d
Jimmy Givans <jauyoma.dev@gmail.com>
2024-08-27 15:34:41 +0300

    refactor: make System an abstract class

--- src/Terminal/System.hpp
+++ src/Terminal/System.hpp
@@ -37,7 +37,7 @@ public:
    * @param buffer The buffer being read to
    * @param nbytes The number of bytes to read
    */
-  virtual void read(int fd, void* buffer, std::size_t nbytes);
+  virtual void read(int fd, void* buffer, std::size_t nbytes) = 0;
 
   /**
    * @brief Write n bytes of buffer to fd
@@ -46,7 +46,7 @@ public:
    * @param buffer The buffer being written from
    * @param nbytes The number of bytes to be written
    */
-  virtual void write(int fd, void const* buffer, std::size_t nbytes);
+  virtual void write(int fd, void const* buffer, std::size_t nbytes) = 0;
   virtual ~System();
 };
 
