824449ac20647833b6630cd1340975cbc52830f2
Jimmy Givans <jauyoma.dev@gmail.com>
2024-10-17 14:32:14 +0300

    refactor: pass in the deleter as a const&

--- src/Editor/config/config.hpp
+++ src/Editor/config/config.hpp
@@ -60,7 +60,7 @@ private:
   };
 
   [[no_unique_address]] TerminalModeDeleter const deleter;
-  std::unique_ptr<terminal::TerminalMode, TerminalModeDeleter> m_mode {new terminal::TerminalMode(), deleter};
+  std::unique_ptr<terminal::TerminalMode, TerminalModeDeleter const&> m_mode {new terminal::TerminalMode(), deleter};
 
   std::vector<std::string> m_row;
   std::vector<std::string> m_render;
