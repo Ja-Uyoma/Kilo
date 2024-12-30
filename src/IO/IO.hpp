a021845d5eaaf940a8420be814a1c909b12952dd
Jimmy Givans <jauyoma.dev@gmail.com>
2024-12-30 12:42:18 +0300

    feat: add function readKey to the IO.hpp and IO.cpp source files

---
+++ src/IO/IO.hpp
@@ -0,0 +1,49 @@
+/**
+ * MIT License
+ * Copyright (c) 2023 Jimmy Givans
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef IO_HPP
+#define IO_HPP
+
+namespace Kilo::IO {
+
+/**
+ * \brief Read key input from stdin
+ * \return The character read
+ * \throws std::system_error if an error occured during read
+ */
+int readKey();
+
+namespace detail {
+
+/**
+ * \brief Handle the processing of escape sequences read in from stdin
+ *
+ * \return unsigned The key representing the input escape sequence
+ */
+unsigned handleEscapeSequences() noexcept;
+
+}   // namespace detail
+
+}   // namespace Kilo::IO
+
+#endif
