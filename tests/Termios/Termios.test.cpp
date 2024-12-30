70e715e6812b801bf3e1433128f401f3450f39c3
Jimmy Givans <jauyoma.dev@gmail.com>
2024-12-30 11:36:11 +0300

    test: add test cases for function ttyRaw

--- tests/Termios/Termios.test.cpp
+++ tests/Termios/Termios.test.cpp
@@ -1,5 +1,6 @@
 #include "Termios/Termios.hpp"
 
+#include <gsl/util>
 #include <gtest/gtest.h>
 
 namespace Kilo::Terminal {
@@ -18,6 +19,28 @@ TEST(getTerminalDriverSettings, Runs_Successfully_When_Passed_A_Valid_File_Descr
   int fd = STDIN_FILENO;
 
   ASSERT_NO_THROW(getTerminalDriverSettings(fd, buf));
+}
+
+TEST(ttyRaw, Throws_An_Exception_When_Passed_An_Invalid_File_Descriptor)
+{
+  ::termios buf;
+  ::termios copy;
+  int fd = STDOUT_FILENO;
+
+  getTerminalDriverSettings(fd, buf);
+  ASSERT_THROW(ttyRaw(-fd, buf, copy), std::system_error);
+}
+
+TEST(ttyRaw, Succeeds_When_Passed_A_Valid_File_Descriptor)
+{
+  ::termios buf;
+  ::termios copy;
+  int fd = STDIN_FILENO;
+  auto cleanup = gsl::finally([&fd, &buf] { ttyReset(fd, buf); });
+
+  getTerminalDriverSettings(fd, buf);
+
+  ASSERT_NO_THROW(ttyRaw(fd, buf, copy));
 }
 
 }   // namespace Kilo::Terminal
