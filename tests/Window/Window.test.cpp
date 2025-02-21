823b8d7e927cda5bb58e40a3d517270a574134b9
Jimmy Givans <jauyoma.dev@gmail.com>
2025-02-21 18:09:20 +0300

    test: use Window's default constructor to create instances of it in tests

--- tests/Window/Window.test.cpp
+++ tests/Window/Window.test.cpp
@@ -7,14 +7,14 @@ namespace Kilo::Terminal {
 
 TEST(Window, RowsReturnsAPositiveNumberOfRows)
 {
-  Window w = Window::create();
+  Window w;
 
   ASSERT_THAT(w.rows(), ::testing::Gt(0));
 }
 
 TEST(Window, ColsReturnsAPositiveNumberOfColumns)
 {
-  Window w = Window::create();
+  Window w;
 
   ASSERT_THAT(w.cols(), ::testing::Gt(0));
 }
