830aa6ed729a387be7d18b0bd8c0df69b8aebb7c
Jimmy Givans <jauyoma.dev@gmail.com>
2025-04-30 17:38:22 +0300

    fix: properly order the operations in the changesDidNotStick() lambda

--- src/Terminal/TerminalMode/TerminalMode.cpp
+++ src/Terminal/TerminalMode/TerminalMode.cpp
@@ -137,8 +137,8 @@ void ttyRaw(int fileDescriptor, termios const& buf, termios& copy)
   }
 
   auto const changesDidNotStick = [&copy] {
-    return (copy.c_iflag & (BRKINT | ICRNL | INPCK | ISTRIP | IXON) != 0) || (copy.c_oflag & OPOST) != 0
-        || ((copy.c_cflag & CS8) != CS8) || (copy.c_lflag & (ECHO | ICANON | IEXTEN | ISIG) != 0) || (copy.c_cc[VMIN] != 0)
+    return (copy.c_iflag & (BRKINT | ICRNL | INPCK | ISTRIP | IXON))  != 0 || (copy.c_oflag & OPOST) != 0
+        || ((copy.c_cflag & CS8) != CS8) || (copy.c_lflag & (ECHO | ICANON | IEXTEN | ISIG)) != 0 || (copy.c_cc[VMIN] != 0)
         || (copy.c_cc[VTIME] != 1);
   };
 
