29b3667a1edda3e2169e5a18bfe72039dd7c2d19
Jimmy Givans <jauyoma.dev@gmail.com>
2026-04-04 16:26:27 +0300

    style: use Javadoc-style comment blocks

--- src/kilo/Application/Application.hpp
+++ src/kilo/Application/Application.hpp
@@ -36,16 +36,16 @@ namespace kilo::application {
 class application final
 {
 public:
-  ///
-  /// \brief Default constructor
-  ///
+  /**
+   * @brief Default constructor
+   */
   explicit application() noexcept(false);
 
-  ///
-  /// \brief Run the application with the given command-line arguments
-  /// \param[in] args The command-line arguments passed to the application
-  /// \returns EXIT_SUCCESS on success, and EXIT_FAILURE otherwise
-  ///
+  /**
+   * @brief Run the application with the given command-line arguments
+   * @param[in] args The command-line arguments passed to the application
+   * @returns EXIT_SUCCESS on success, and EXIT_FAILURE otherwise
+   */
   static auto main(std::span<char const*> args) -> int;
 
 private:
@@ -53,17 +53,16 @@ private:
   kilo::editor::append_buffer::append_buffer m_abuf;
   kilo::terminal::window_size::window_size m_winsize;
 
-  ///
-  /// \brief Open a file and write its contents to memory
-  ///
-  /// \param[in] path The path to the file
-  /// \throws std::ios_base::failure if the file could not be opened or if there was an error while reading from it
-  ///
+  /**
+   * @brief Open a file and write its contents to memory
+   * @param[in] path The path to the file
+   * @throws std::ios_base::failure if the file could not be opened or if there was an error while reading from it
+   */
   void open(std::filesystem::path const& path);
 
-  ///
-  /// \brief Run the application
-  ///
+  /**
+   * @brief Run the application
+   */
   void run();
 };
 
